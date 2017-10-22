9cff387c71c5c3d75e547afb306ade51164e1cdb
Rachit Singh <rachitsingh@outlook.com>
2017-10-22 19:11:36 -0400

    Updated dotfiles a bit

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,5 @@
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+export PATH="$PATH:~/.cargo/bin/"
+export PATH="$PATH:~/opt/gcc-arm-none-eabi-6-2017-q2-update/bin/"
--- system/aliases.zsh
+++ system/aliases.zsh
@@ -8,3 +8,6 @@ then
   alias ll="gls -l --color"
   alias la='gls -A --color'
 fi
+
+alias clearcache="sudo dscacheutil -flushcache"
+alias tmux="TERM=screen-256color-bce tmux -2"
